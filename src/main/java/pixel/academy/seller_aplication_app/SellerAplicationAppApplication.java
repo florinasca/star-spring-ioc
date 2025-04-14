4191bb027a0866b3563760806666b6ca557f7d8d
florin <florinasca@mail.ru>
2025-04-14 19:07:02 +0300

    Initial commit.

---
+++ src/main/java/pixel/academy/seller_aplication_app/SellerAplicationAppApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.seller_aplication_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerAplicationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerAplicationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/seller_aplication_app/SellerAplicationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.seller_aplication_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerAplicationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
