70c8d1785e4819f5347eb503e53498ad1e645aaa
florin <florinasca@mail.ru>
2025-04-17 17:50:01 +0300

    Initial commit.

--- src/main/java/pixel/academy/seller_aplication_app/rest/DemoController.java
+++ src/main/java/pixel/academy/seller_aplication_app/rest/DemoController.java
@@ -1,7 +1,8 @@
 package pixel.academy.seller_aplication_app.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 //constructor injection
 
 
-public class DemoController {
-}
+//public class DemoController {
